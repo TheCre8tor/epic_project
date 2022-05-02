c5aeba758b5550eda2b63eaa8403970036b107c0
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-02 14:48:38 +0100

    Initialize an account and also retrieved the account details

--- programs/epic_project/src/lib.rs
+++ programs/epic_project/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 // This is the [program id],
 // This address is the deployment address
 // solana address -k target/deploy/epic_project-keypair.json to check
-declare_id!("CdXzRnzedF2v1j5YU4BiMBR3Zsgc5tXvRms3qkTW5zan");
+declare_id!("Fdw4Hsmd11AaKDAsuEHG1FAqTCUGiJ97g7WXVT8hd28h");
 
 // #[program] this syntax is called a macros
 // and it's semilar to inheriting a class.
@@ -15,7 +15,16 @@ pub mod epic_project {
     
     use super::*;
 
-    pub fn start_stuff_off(_ctx: Context<StartStuffOff>) -> Result<()> {
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+        // Get a reference to the account.
+        /* We do &mut to get a "mutable reference" to base_account. 
+           When we do this it actually gives us the power to make changes 
+           to base_account.  */
+        let base_account = &mut ctx.accounts.base_account;
+
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
+
         Ok(())
     }
 }
@@ -23,4 +32,39 @@ pub mod epic_project {
 /* Here, we'll basically be able to specify 
    different account constraints. */
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    /* All we're doing here is telling Solana how we 
+       want to initialize BaseAccount. 
+       
+       1. init -> will tell Solana to create a new account 
+          owned by our current program.
+       2. [payer = user] -> tells our program who's paying for 
+          the account to be created. In this case, it's the 
+          user calling the function.
+       3. We then say [space = 9000] -> which will allocate 9000 
+          bytes of space for our account. You can change 
+          this # if you wanted, but, 9000 bytes is enough 
+          for the program we'll be building here! 
+    */
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    /* This data is passed into the program that proves to the 
+       program that the user calling this program actually owns 
+       their wallet account. */
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    /* SystemProgram is the program that basically runs Solana. 
+       It is responsible for a lot of stuff, but one of the main 
+       things it does is create accounts on Solana. The 
+       SystemProgram is a program the creators of Solana deployed 
+       that other programs like ours talk to haha — it has an id 
+       of 11111111111111111111111111111111. */
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- tests/epic_project.ts
+++ tests/epic_project.ts
@@ -2,23 +2,46 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { EpicProject } from "../target/types/epic_project";
 
+const { SystemProgram } = anchor.web3;
+
 describe("epic_project", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  /* We set it before but we needed to update it, so 
+     that it can communicate with our frontend! */
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.EpicProject as Program<EpicProject>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    // const tx = await program.methods.initialize().rpc();
-    // console.log("Your transaction signature", tx);
-
     console.log("🚀 Starting test...");
+
+    // Create an account keypair for our program to use.
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    // Call start_stuff_off, pass it the params it needs!
+    let tx = await program.methods
+      .startStuffOff()
+      .accounts({
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([baseAccount])
+      .rpc();
+
+    console.log("📝 Your transaction signature", tx);
+
+    // Fetch data from the account.
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+
+    console.log("👀 GIF Count", account.totalGifs.toString());
+    console.log("Wallet Address: ", provider.wallet.publicKey.toString());
 
     // We call our method and wait for our local
     // validator to "mine" the instruction.
-    const tx = await program.methods.startStuffOff().rpc();
-
-    console.log("📝 Your transaction signature", tx);
+    // const tx = await program.methods.startStuffOff().rpc();
   });
 });
